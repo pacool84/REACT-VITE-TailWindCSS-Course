11302ae68631617de6161a6386f0ac78f49ea883
Pacool84 <flopez_1384@hotmail.com>
2023-06-22 19:56:23 -0600

    [ADD] El componente de Home

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import Home from "../Home/index";
 
 function App() {
   return (
     <div className="bg-red-100">
       <h1>Hello my Friend REACT with VITE</h1>
+      <Home />
     </div>
   );
 }
---
+++ src/Home/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Home() {
+  return <h1>Hello my friend HOME component</h1>;
+}
+
+export default Home;
