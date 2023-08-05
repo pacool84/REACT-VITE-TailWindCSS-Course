cb25f3957bcdcb09866e274ba3a82fb18064282f
Pacool84 <flopez_1384@hotmail.com>
2023-08-05 16:37:38 -0600

    [ADD] El componente de card y sus estilos

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Layout from "../Components/Layout";
+import Card from "../Components/Card";
 
 function Home() {
-  return <Layout>Home</Layout>;
+  return (
+    <Layout>
+      Home
+      <Card />
+    </Layout>
+  );
 }
 
 export default Home;
