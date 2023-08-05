fc21e49d2f40a4df1f9f06bc0d47a16e22bdaa89
Pacool84 <flopez_1384@hotmail.com>
2023-08-05 17:42:16 -0600

    [ADD] consumo de API y renderizado de productos

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,24 +1,24 @@
 import React from "react";
 
-const Card = () => {
+const Card = (data) => {
   return (
     <div className=" bg-white cursor-pointer w-56 h-60 rounded-lg ">
       <figure className=" relative mb-2 w-full h-4/5 ">
         <span className=" absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5 ">
-          Electronics
+          {data.data.category.name}
         </span>
         <img
           className=" w-full h-full object-cover rounded-lg "
-          src="https://images.pexels.com/photos/439227/pexels-photo-439227.jpeg?auto=compress&cs=tinysrgb&w=800"
-          alt="headphones"
+          src={data.data.images[0]}
+          alt={data.data.title}
         />
         <div className=" absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1 ">
           +
         </div>
       </figure>
       <p className=" flex justify-between ">
-        <span className=" text-sm font-light">Headphones</span>
-        <span className=" text-lg font-medium">$300</span>
+        <span className=" text-sm font-light">{data.data.title}</span>
+        <span className=" text-lg font-medium">${data.data.price}</span>
       </p>
     </div>
   );
--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import Layout from "../Components/Layout";
 import Card from "../Components/Card";
 
 function Home() {
+  const [items, setItems] = useState(null);
+  /* Consumo de la API */
+
+  useEffect(() => {
+    fetch("https://api.escuelajs.co/api/v1/products")
+      .then((response) => response.json())
+      .then((data) => setItems(data));
+  }, []);
+
   return (
     <Layout>
       Home
-      <Card />
+      <div className=" grid gap-3  grid-cols-4 w-full max-w-screen-lg ">
+        {items?.map((item) => {
+          return <Card key={item.id} data={item} />;
+        })}
+      </div>
     </Layout>
   );
 }
