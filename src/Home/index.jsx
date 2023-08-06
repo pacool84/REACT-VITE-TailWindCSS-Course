4c2052bc282cb0bb072dd596d4e72883411388d4
Pacool84 <flopez_1384@hotmail.com>
2023-08-05 18:15:51 -0600

    [CHANGE] A la estructura del llamado a la API

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -2,14 +2,25 @@ import { useState, useEffect } from "react";
 import Layout from "../Components/Layout";
 import Card from "../Components/Card";
 
+const url = "https://api.escuelajs.co/api/v1/products";
+
 function Home() {
   const [items, setItems] = useState(null);
   /* Consumo de la API */
 
+  const fetchProducts = async () => {
+    try {
+      const response = await fetch(url);
+      const items = await response.json();
+
+      setItems(items);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   useEffect(() => {
-    fetch("https://api.escuelajs.co/api/v1/products")
-      .then((response) => response.json())
-      .then((data) => setItems(data));
+    fetchProducts();
   }, []);
 
   return (
