ce59f7d568d91c53d5dfb3e6e99c81c0ecc18f63
Pacool84 <flopez_1384@hotmail.com>
2023-08-28 21:14:09 -0600

    [FIX] Al pase de props utilizando destructuring

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -28,7 +28,7 @@ function Home() {
       Home
       <div className=" grid gap-3  grid-cols-4 w-full max-w-screen-lg ">
         {items?.map((item) => {
-          return <Card key={item.id} data={item} />;
+          return <Card key={item.id} {...item} />;
         })}
       </div>
     </Layout>
