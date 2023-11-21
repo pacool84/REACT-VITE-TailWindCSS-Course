32d4fd929630c8fa05bb867f753a536f2705c5e3
Pacool84 <flopez_1384@hotmail.com>
2023-11-21 06:35:15 -0600

    [CHANGE] Al manejo del evento onClick para mejor entendimiento del codigo

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -4,6 +4,10 @@ import { ShoppingCartContext } from "../../Context";
 const Card = ({ category: { name }, images, title, price }) => {
   /* context podria ser cualquier nombre */
   const context = useContext(ShoppingCartContext);
+
+  const incrementCart = () => {
+    context.setCount(context.count + 1);
+  };
   return (
     <div className=" bg-white cursor-pointer w-56 h-60 rounded-lg ">
       <figure className=" relative mb-2 w-full h-4/5 ">
@@ -17,7 +21,10 @@ const Card = ({ category: { name }, images, title, price }) => {
         />
         <div
           className=" absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1 "
-          onClick={() => context.setCount(context.count + 1)}
+          /* onClick={() => context.setCount(context.count + 1)} */
+          onClick={
+            incrementCart
+          } /* Refactorizando el onClick para hacerlo mas entendible */
         >
           +
         </div>
