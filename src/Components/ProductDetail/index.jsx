24b32d17b0e2192a73e0863baaeef98b39304f77
Pacool84 <flopez_1384@hotmail.com>
2023-11-25 18:14:35 -0600

    [ADD] Logica al contexto para poder controlar la vista del componente ProductDetail desde el componente Card

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,13 +1,24 @@
+import { useContext } from "react";
+import { ShoppingCartContext } from "../../Context";
 import "./styles.css";
 import { XMarkIcon } from "@heroicons/react/24/solid";
 
 const ProductDetail = () => {
+  const context = useContext(ShoppingCartContext);
   return (
-    <aside className="product-detail flex flex-col fixed right-0 border border-black rounded-lg bg-white">
+    <aside
+      className={`${context.isProductDetailOpen ? "flex" : "hidden"}
+      product-detail flex-col fixed right-0 border border-black rounded-lg bg-white`}
+    >
       <div className="flex justify-between items-center ">
         <h2 className="font-medium text-xl p-6">Detail</h2>
         <div>
-          <XMarkIcon className="h-6 w-6 text-black" />
+          <XMarkIcon
+            className="h-6 w-6 text-black"
+            onClick={() => {
+              context.closeProductDetail();
+            }}
+          />
         </div>
       </div>
     </aside>
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -5,12 +5,24 @@ export const ShoppingCartContext = createContext();
 export const ShoppingCartProvider = ({ children }) => {
   const [count, setCount] = useState(0);
   /* console.log("COUNT", count); */
+  const [isProductDetailOpen, setIsProductDetailOpen] = useState(false);
+
+  const openProductDetail = () => {
+    setIsProductDetailOpen(true);
+  };
+
+  const closeProductDetail = () => {
+    setIsProductDetailOpen(false);
+  };
 
   return (
     <ShoppingCartContext.Provider
       value={{
         count,
         setCount,
+        openProductDetail,
+        closeProductDetail,
+        isProductDetailOpen,
       }}
     >
       {children}
