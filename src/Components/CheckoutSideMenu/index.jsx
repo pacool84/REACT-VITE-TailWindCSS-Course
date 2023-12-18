88693c94cff9272319443f433154cf72a41aa1cd
Pacool84 <flopez_1384@hotmail.com>
2023-12-18 05:05:02 -0600

    [ADD] Logica para eliminar articulos del componente CheckoutSideMenu

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -8,6 +8,13 @@ const CheckoutSideMenu = () => {
   const context = useContext(ShoppingCartContext);
   console.log("Producto con detalles ", context.productToShow);
   console.log("CART: ", context.cartProducts);
+
+  const handleDelete = (id) => {
+    const filteredProducts = context.cartProducts.filter(
+      (product) => product.id != id
+    );
+    context.setCartProducts(filteredProducts);
+  };
   return (
     <aside
       className={`${context.isCheckoutSideMenuOpen ? "flex" : "hidden"}
@@ -29,9 +36,11 @@ const CheckoutSideMenu = () => {
         {context.cartProducts.map((product) => (
           <OrderCard
             key={product.id}
+            id={product.id}
             title={product.title}
             imageUrl={product.images}
             price={product.price}
+            handleDelete={handleDelete}
           />
         ))}
       </div>
