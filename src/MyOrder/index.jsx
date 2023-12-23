1ff8afbce91f3533997fedba3bcf8a8df1ad9930
Pacool84 <flopez_1384@hotmail.com>
2023-12-23 11:15:09 -0600

    [ADD] El componente OrdersCard, logica para pintar cada una de las ordenes

--- src/MyOrder/index.jsx
+++ src/MyOrder/index.jsx
@@ -1,5 +1,7 @@
 import { useContext } from "react";
 import { ShoppingCartContext } from "../Context";
+import { Link } from "react-router-dom";
+import { ChevronLeftIcon } from "@heroicons/react/24/solid";
 import Layout from "../Components/Layout";
 import OrderCard from "../Components/OrderCard";
 
@@ -7,8 +9,13 @@ function MyOrder() {
   const context = useContext(ShoppingCartContext);
   return (
     <Layout>
-      My Order
-      <div className="px-6 overflow-y-scroll flex-1 ">
+      <div className="flex w-80 items-center relative justify-center mb-6">
+        <Link to="/my-orders" className="absolute left-0 ">
+          <ChevronLeftIcon className="h-6 w-6 text-black cursor-pointer" />
+        </Link>
+        <h1>My Order</h1>
+      </div>{" "}
+      <div className="flex flex-col w-80">
         {context.order?.slice(-1)[0].products.map(
           (
             product //Aqui solo queremos renderizar la ultima porcion de la orden
