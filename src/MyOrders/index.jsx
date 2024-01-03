2e5c3f909a4054dacc08f99c9aa1a3c72c0f86f8
Pacool84 <flopez_1384@hotmail.com>
2024-01-02 22:20:43 -0600

    [ADD] Estilos al componente de OrdersCard y MyOrders

--- src/MyOrders/index.jsx
+++ src/MyOrders/index.jsx
@@ -8,8 +8,8 @@ function MyOrders() {
   const context = useContext(ShoppingCartContext);
   return (
     <Layout>
-      <div className="flex w-80 items-center relative justify-center">
-        <h1>My Orders</h1>
+      <div className="flex w-80 items-center relative justify-center mb-4">
+        <h1 className="font-medium text-xl">My Orders</h1>
       </div>
       {context.order.map((order, index) => (
         <Link key={index} to={`/my-orders/${index}`}>
@@ -17,7 +17,6 @@ function MyOrders() {
             totalPrice={order.totalPrice}
             totalProducts={order.totalProducts}
           />
-          ;
         </Link>
       ))}
     </Layout>
